e2d54dfa3f628a06b5696c6058fce33836cb69de
aam505 <andreea.muresan505@gmail.com>
2019-05-31 00:00:46 +0200

    added-tattoo
    added body tattoo ooverlay for male and female

---
+++ Assets/Scripts/Males/UmaMale2.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UMA.CharacterSystem;
+public class UmaMale2 : MonoBehaviour
+{
+    private DynamicCharacterAvatar avatar;
+    private Dictionary<string, DnaSetter> dna;
+    // Start is called before the first frame update
+    void Start()
+    {
+        avatar = GetComponent<DynamicCharacterAvatar>();
+        try { dna = avatar.GetDNA(); }
+        catch (System.Exception e)
+        {
+            Debug.Log(e.Message);
+        }//takes couple of frames 
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+         
+            avatar.LoadFromTextFile("male2");
+            avatar.BuildCharacter();
+        }
+    }
+}
