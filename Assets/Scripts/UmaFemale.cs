578e69e909676dfdc8fc177f02b439de3c7a60b9
aam505 <andreea.muresan505@gmail.com>
2019-06-28 12:14:43 +0200

    - female survey pics

--- Assets/Scripts/UmaFemale.cs
+++ Assets/Scripts/UmaFemale.cs
@@ -52,24 +52,17 @@ public class UmaFemale : MonoBehaviour
 
         if (connected)
         {
-            expression.mouthUp_Down = -0.35f;
+            expression.mouthUp_Down = 0.4f;
             expression.browsIn = 1f;
-            expression.noseSneer = 0f;
             expression.leftBrowUp_Down = 0.4f;
             expression.rightBrowUp_Down = 0.4f;
+            expression.midBrowUp_Down = 0f;
+
         }
 
         dna = avatar.GetDNA(); //takes couple of frames 
-        //if (Input.GetKeyDown(KeyCode.Alpha1))
-        //{
-        //    avatar.SetSlot("Chest", "MaleHoodie_Recipe");
-        //    avatar.BuildCharacter();
-        //}
-        //if (Input.GetKeyDown(KeyCode.Alpha0))
-        //{
-        //    avatar.ClearSlot("Chest");
-        //    avatar.BuildCharacter();
-        //}
+    
+
         if (Input.GetKeyDown(KeyCode.Alpha1)) //load features from file
         {
 
@@ -79,7 +72,7 @@ public class UmaFemale : MonoBehaviour
 
         }
 
-        if (Input.GetKeyDown(KeyCode.Alpha2)) //adjust and normalize buff 
+        if (Input.GetKeyDown(KeyCode.Alpha2)) //BUFF CONDITION
         {
             if (dna == null)
             {
@@ -91,11 +84,11 @@ public class UmaFemale : MonoBehaviour
             avatar.characterColors.SetColor("Shirt", Color.black);
 
 
-            if (name.Equals("UMA_M6") || name.Equals("UMA_M7"))
+            if (name.Equals("UMA_F3") || name.Equals("UMA_F4"))
             {
-                avatar.characterColors.SetColor("ClothingTop01", Color.black);
-
-                avatar.characterColors.SetColor("ClothingBottom01", new Color(20f / 255f, 18f / 255f, 36f / 255f));
+                avatar.characterColors.SetColor("Footwear01", new Color(27f / 255f, 27f / 255f, 27f / 255f));
+                avatar.characterColors.SetColor("ClothingBottom01", new Color(53f / 255f, 53f / 255f, 77f / 255f));
+                avatar.characterColors.SetColor("ClothingTop01", new Color(29f / 255f, 28f / 255f, 36f / 255f));
             }
             else
             {
@@ -110,10 +103,12 @@ public class UmaFemale : MonoBehaviour
             dna["chinSize"].Set(1f);
             dna["headWidth"].Set(0.850f);
             dna["jawsPosition"].Set(0f);
-            dna["jawsSize"].Set(0.65f);
+            dna["jawsSize"].Set(1f);
             dna["noseSize"].Set(0.38f);
             dna["noseWidth"].Set(0.4f);
-            dna["lipsSize"].Set(0.250f);
+            dna["lipsSize"].Set(0);
+ 
+            
 
             dna["armWidth"].Set(1f);
             dna["forearmWidth"].Set(1f);
@@ -122,15 +117,23 @@ public class UmaFemale : MonoBehaviour
             dna["upperMuscle"].Set(0.7f);
             dna["upperWeight"].Set(0.7f);
 
-            dna["eyeSize"].Set(0.5f);
+            dna["eyeSize"].Set(0.4f);
 
             dna["cheekPosition"].Set(0.5f);
             dna["lowCheekPosition"].Set(0.5f);
             //dna["lowCheekPronounced"].Set(0.5f);
             dna["cheekSize"].Set(0.5f);
             dna["mouthSize"].Set(0.5f);
-            dna["lowerMuscle"].Set(0.5f);
+            dna["lowerMuscle"].Set(0.3f);
 
+            dna["noseInclination"].Set(0.47f);
+            dna["nosePosition"].Set(0.362f);
+            dna["noseSize"].Set(1);
+            dna["noseSize"].Set(0.594f);
+            dna["noseWidth"].Set(0.4f);
+            dna["foreheadSize"].Set(1);
+            dna["breastSize"].Set(0.3f);
+            dna["breastCleavage"].Set(0.7f);
 
             avatar.BuildCharacter();
 
@@ -144,16 +147,16 @@ public class UmaFemale : MonoBehaviour
                 dna = avatar.GetDNA();
             }
 
-            if (name.Equals("UMA_M6") || name.Equals("UMA_M7"))
+            if (name.Equals("UMA_F3") || name.Equals("UMA_F4"))
             {
-                avatar.characterColors.SetColor("ClothingTop01", Color.gray);
-
+                avatar.characterColors.SetColor("Footwear01", new Color(27f / 255f, 27f / 255f, 27f / 255f));
                 avatar.characterColors.SetColor("ClothingBottom01", new Color(120f / 255f, 141f / 255f, 183f / 255f));
+                avatar.characterColors.SetColor("ClothingTop01", new Color(128f / 255f, 128f / 255f, 128f / 255f));
             }
             else
             {
+                avatar.characterColors.SetColor("Shirt", Color.grey);
                 avatar.characterColors.SetColor("Pants1", new Color(120f / 255f, 141f / 255f, 183f / 255f));
-                avatar.characterColors.SetColor("Shirt", Color.gray);
             }
 
             avatar.characterColors.SetColor("Eyes", new Color(99f / 255f, 67f / 255f, 67f / 255f));
@@ -166,7 +169,7 @@ public class UmaFemale : MonoBehaviour
             dna["jawsSize"].Set(0.5f);
             dna["noseSize"].Set(0.5f);
             dna["noseWidth"].Set(0.5f);
-            dna["lipsSize"].Set(0.5f);
+            dna["lipsSize"].Set(0.4f);
 
             dna["armWidth"].Set(0.5f);
             dna["forearmWidth"].Set(0.5f);
@@ -185,6 +188,17 @@ public class UmaFemale : MonoBehaviour
             dna["mouthSize"].Set(0.5f);
             dna["lowerMuscle"].Set(0.5f);
 
+            dna["noseInclination"].Set(0.47f);
+            dna["nosePosition"].Set(0.362f);
+            dna["noseSize"].Set(1);
+            dna["noseSize"].Set(0.594f);
+            dna["noseWidth"].Set(0.4f);
+            dna["foreheadSize"].Set(1);
+            dna["breastSize"].Set(0.3f);
+            dna["breastCleavage"].Set(0.7f);
+
+
+
             avatar.BuildCharacter();
 
         }
@@ -195,17 +209,16 @@ public class UmaFemale : MonoBehaviour
                 dna = avatar.GetDNA();
             }
 
-            if (name.Equals("UMA_M6") || name.Equals("UMA_M7"))
+            if (name.Equals("UMA_F3") || name.Equals("UMA_F4"))
             {
-                avatar.characterColors.SetColor("ClothingTop01", Color.white);
-
+                avatar.characterColors.SetColor("Footwear01", new Color(27f / 255f, 27f / 255f, 27f / 255f));
                 avatar.characterColors.SetColor("ClothingBottom01", new Color(120f / 255f, 141f / 255f, 183f / 255f));
+                avatar.characterColors.SetColor("ClothingTop01",  Color.white);
             }
             else
             {
-
-                avatar.characterColors.SetColor("Pants1", new Color(120f / 255f, 141f / 255f, 183f / 255f));
                 avatar.characterColors.SetColor("Shirt", Color.white);
+                avatar.characterColors.SetColor("Pants1", new Color(120f / 255f, 141f / 255f, 183f / 255f));
             }
 
 
@@ -219,7 +232,7 @@ public class UmaFemale : MonoBehaviour
             dna["jawsSize"].Set(0.3f);
             dna["noseSize"].Set(0.3f);
             dna["noseWidth"].Set(0.3f);
-            dna["lipsSize"].Set(0.7f);
+            dna["lipsSize"].Set(0.6f);
 
             dna["armWidth"].Set(0.3f);
             dna["forearmWidth"].Set(0.3f);
@@ -238,6 +251,14 @@ public class UmaFemale : MonoBehaviour
             dna["mouthSize"].Set(0.5f);
             dna["lowerMuscle"].Set(0.5f);
 
+            dna["noseInclination"].Set(0.47f);
+            dna["nosePosition"].Set(0.362f);
+            dna["noseSize"].Set(1);
+            dna["noseSize"].Set(0.594f);
+            dna["noseWidth"].Set(0.4f);
+            dna["foreheadSize"].Set(1);
+            dna["breastSize"].Set(0.3f);
+            dna["breastCleavage"].Set(0.7f);
 
             avatar.BuildCharacter();
         }
