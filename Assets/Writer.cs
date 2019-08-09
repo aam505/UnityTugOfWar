0caca8b2aa2dedee49b2ddaea23be89505bf4f34
aam505 <andreea.muresan505@gmail.com>
2019-08-09 16:56:52 +0200

    - demo

--- Assets/Writer.cs
+++ Assets/Writer.cs
@@ -19,7 +19,6 @@ public class Writer : MonoBehaviour
 
     void Start()
     {
-        Debug.Log("Started");
         logData = new LogData();
         initTimestamp = DateTime.Now.ToString("s").Replace(':','-');
        
